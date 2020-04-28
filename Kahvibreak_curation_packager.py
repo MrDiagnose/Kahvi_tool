538671a4ae6bebb49c13e0cc817c5f32734efcf7
MrDiagnose <johndripper5@gmail.com>
2020-04-28 21:00:18 +0530

    Added Test button

--- Kahvibreak_curation_packager.py
+++ Kahvibreak_curation_packager.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import os
+import subprocess
 import shutil #to copy files
 from tkinter.filedialog import *
 from PIL import ImageTk, Image
@@ -13,6 +14,9 @@ def metadata_window():
     top.iconbitmap('resources/icon.ico')
     top.geometry('600x500')
     top.resizable(False, False)
+    
+    
+    
     
     def cal_scale():
         height=game_height.get()
@@ -69,8 +73,65 @@ def metadata_window():
             os.replace('temp/game','temp/'+game_name_entry.get() )
         shutil.make_archive('output/'+game_name_entry.get(),'zip','temp')
         root.destroy() #exit the window
+        os.system('cmd /k "start output"') #opens output folder
         
+    #--------------------------test game routine----------------------------------------
+    dir_file=open('resources/kahvi_config.txt','r')
+    soft_dir=dir_file.readlines()
+    
+    try:
+        global freej2me_dir, freej2me_new_dir,kemulator_1_0_3_dir, kemulator_9_8_dir,kemulator_9_8_dir,Jre_1_4_dir,jre_1_8_dir
+        freej2me_dir=soft_dir[0].strip()
+        freej2me_new_dir=soft_dir[1].strip()
+        kemulator_1_0_3_dir=soft_dir[2].strip()
+        kemulator_9_8_dir=soft_dir[3].strip()
+        jre_1_4_dir=soft_dir[4].strip()
+        jre_1_8_dir=soft_dir[5].strip()
+        print(freej2me_dir+'\n'+
+          freej2me_new_dir)
+    except IndexError:
+        print("Pls select kahvibreak directory")
+   
+    dir_file.close()
 
+    def select_kahvi_dir():
+        try:
+            kahvi_dir =  askdirectory()
+        except AttributeError:
+                 print("No directory selected")
+        else:
+            kahvi_config=open('resources/kahvi_config.txt','w')
+            kahvi_config.write(kahvi_dir+"/Software/FreeJ2ME/freej2me-kahvi-0.4.1.jar"+"\n"
+                           +kahvi_dir+"/Software/FreeJ2ME/freej2me-kahvi-0.4.2.jar"+"\n"
+                            +kahvi_dir+"/Software/KEmulator/KEmulator-1.0.3.jar"+"\n"
+                            +kahvi_dir+"/Software/KEmulator/KEmulator-0.9.8.jar"+"\n"
+                            +kahvi_dir+"/Software/jre-1.4/bin/javaz.exe"+"\n"
+                            +kahvi_dir+"/Software/jre-8/bin/javaz.exe")
+            kahvi_config.close()
+    def test_game_btn():
+        emulator_var=emulator.get()
+        batch_script=''
+        if emulator_var=='Software\FreeLaunch.bat':
+            batch_script='start "" "' + jre_1_8_dir+'" -jar "'+freej2me_dir+'" "file:///'+game_file_src+'" '+game_width.get()+" "+game_height.get()+" "+zoom_level.get()+" "+fps.get()
+
+        elif emulator_var=='Software\FreeLaunch0.4.2.bat':
+            batch_script='start "" "' + jre_1_8_dir+'" -jar "'+freej2me_new_dir+'" "file:///'+game_file_src+'" '+game_width.get()+" "+game_height.get()+" "+zoom_level.get()+" "+fps.get()
+
+        elif emulator_var=='Software\KLaunch.bat':
+            batch_script='start "" "' + jre_1_8_dir+'" -jar "'+kemulator_1_0_3_dir+'" -jar "'+game_file_src
+
+        #emulator='Software\KLaunch98.bat'
+        else:
+            batch_script='start "" "' + jre_1_4_dir+'" -jar "'+kemulator_9_8_dir+'" -jar "'+game_file_src
+
+        bat_file=open('resources/run_game.bat','w')
+        bat_file.write(batch_script)
+        bat_file.close()
+        #run_game_script='"'+os.path.abspath('resources/run_game.bat')+'"'
+        run_game_script=os.path.abspath('resources/run_game.bat')
+        subprocess.call(run_game_script)
+
+    #-----------------------------------------------------------------
 
     LabelList=[0]*13
     LabelListText=['Title','Series','Developer','Publisher','Play Mode','Status','Extreme','Genre','Source','Emulator to use','Launch settings','Notes','Author notes']
@@ -142,9 +203,9 @@ def metadata_window():
     freej2me.pack(side=LEFT)
     freej2me_new=Radiobutton(frame5,text='FreeJ2me(new)', var=emulator,value='Software\FreeLaunch0.4.2.bat')
     freej2me_new.pack(side=LEFT)
-    kemulator_new=Radiobutton(frame5,text='Kemulator 1.0.3', var=emulator,value='Software\KLaunch.bat')
+    kemulator_new=Radiobutton(frame5,text='Kemulator v1.0.3', var=emulator,value='Software\KLaunch.bat')
     kemulator_new.pack(side=LEFT)
-    kemulator_old=Radiobutton(frame5,text='Kemulator 9.8', var=emulator,value='Software\KLaunch98.bat')
+    kemulator_old=Radiobutton(frame5,text='Kemulator v9.8', var=emulator,value='Software\KLaunch98.bat')
     kemulator_old.pack(side=LEFT)
     
 
@@ -154,9 +215,13 @@ def metadata_window():
     frame6.grid(row=10,column=1)
     
     
+
+    sel_kahvi_dir_btn=Button(frame6,text='Select Kahvi Direcotry',command=select_kahvi_dir)
+    sel_kahvi_dir_btn.grid(row=0,column=2,padx=10)
+    test_game_btn=Button(frame6,text='Test Game',command=test_game_btn)
+    test_game_btn.grid(row=1,column=2,padx=10)
     cal_scale_btn=Button(frame6,text='Cal Scale',command=cal_scale)
     cal_scale_btn.grid(row=2,column=2,padx=10)
-    
     
     game_width_label=Label(frame6, text='Width')
     game_width_label.grid(row=0,column=0)
@@ -263,11 +328,14 @@ def copy_image_files(source,destination):
 
     
 root=Tk()
-root.title('Kahvibreak Curation Tool v1.0')
+root.title('Kahvibreak Curation Tool v1.1')
 root.iconbitmap('resources/icon.ico')
 root.geometry('500x500')
 root.resizable(False, False)
 
+#color
+#window_one_bg='black'
+#window_one_fg='white'
 
 image_frame_master=Frame()
 image_frame_master.pack(side=TOP)
@@ -293,6 +361,8 @@ screenshot_view_default.grid(row=0,column=0)
 buttons_frame=LabelFrame(root,text='buttons',padx=10,pady=10)
 buttons_frame.pack(side=BOTTOM)
 
+
+#open_logo=Button(buttons_frame,bg=window_one_bg,fg=window_one_fg,font=('Verdana', 10),text="Select Logo", command = lambda: open_image(1))
 open_logo=Button(buttons_frame,font=('Verdana', 10),text="Select Logo", command = lambda: open_image(1))
 open_logo.pack(fill=X)
 
